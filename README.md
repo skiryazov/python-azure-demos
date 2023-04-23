52a19f47c5b71d02ba0aa593c90dd1b75a2d643d
Stefan Kiryazov <stefan.kiryazov@gmail.com>
2023-04-23 21:26:15 +0200

    Added cosmos db demo

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # python-azure-demos
-Simple Python demos for interacting with Azure, intended for following the AZ-204 course
+Simple Python demos for interacting with Azure.
+
+These are intended for following the AZ-204 Azure Developer course but can be used by any Python developer learning the ropes of Azure.
+
+Feel free to contribute.
---
+++ cosmos.py
@@ -0,0 +1,25 @@
+import azure.cosmos.cosmos_client as cosmos_client
+from azure.cosmos.partition_key import PartitionKey
+
+# Initialize the Cosmos DB client
+url = 'https://<INSERT_ACCOUNT>.documents.azure.com:443/'
+key = '<INSERT_key>'
+client = cosmos_client.CosmosClient(url, {'masterKey': key})
+
+# Retrieve all items from the container
+database_name = 'ToDoList' # Change the name if not using the default
+container_name = 'Items' # Change the name if not using the default
+
+db = client.create_database_if_not_exists(id=database_name)
+container = db.create_container_if_not_exists(id=container_name, partition_key=PartitionKey(path='/id', kind='Hash'))
+
+
+print('Reading all items in a container\n')
+
+item_list = list(container.read_all_items(max_item_count=10))
+
+print('Found {0} items'.format(item_list.__len__()))
+
+for doc in item_list:
+    print('Item Id: {0}'.format(doc.get('id')))
+#    print('Name: {0}'.format(doc.get('name')))
